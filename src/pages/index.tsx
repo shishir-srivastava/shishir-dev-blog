4bfcba676eb19d2e43b4a53e78becac4e187b0df
Shishir Srivastava <shisriv@microsoft.com>
2021-05-13 23:54:41 +0530

    Adds TS Config

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+// import { graphql } from "gatsby"
+
+const Home = (): React.ReactElement => {
+  return <div>Hello world! Shishir&apos;s blog is being setup.</div>
+}
+
+export default Home
+
+// export const pageQuery = graphql`
+//   query MetadataQuery {
+//     site {
+//       siteMetadata {
+//         title
+//         description
+//       }
+//     }
+//   }
+// `
