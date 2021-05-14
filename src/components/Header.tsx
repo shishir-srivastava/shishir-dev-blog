7df030b3f3b75d27bed7f865bb0d9d7cc2d8b585
Shishir Srivastava <shisriv@microsoft.com>
2021-05-14 12:44:09 +0530

    Minor updates

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Header = (): React.ReactElement => <h1>Some Title</h1>
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import React from "react"
+import Header from "../components/Header"
 // import { graphql } from "gatsby"
 
 const Home = (): React.ReactElement => {
-  return <div>Hello world! Shishir&apos;s blog is being setup.</div>
+  return (
+    <>
+      <Header />
+      <div>Hello world! Shishir&apos;s blog is being setup.</div>
+    </>
+  )
 }
 
 export default Home
